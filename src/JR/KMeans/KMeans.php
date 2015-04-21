72fda5c22737c84089eeebe9f8c69457fc6ea40c
RebendaJiri <jr.rebenda@rje.cz>
2015-04-21 15:36:57 +0200

    simple cluster method argument value check

--- src/JR/KMeans/KMeans.php
+++ src/JR/KMeans/KMeans.php
@@ -12,12 +12,25 @@ use Nette;
  */
 class KMeans
 {
+	/** @var array */
+	private $data;
+	
+	public function __construct(array $data)
+	{
+		$this->data = $data;
+	}
+	
 	/**
 	 * @param int $clusterCount >= 2
 	 * @return array
 	 */
 	public function cluster($clusterCount)
 	{
-		
+		if ($clusterCount < 2) {
+			throw new \Exception('Cluster count must be equal or greater to 2.');
+		}
+		if ($clusterCount > count($data)) {
+			throw new \Exception('Cluster count cannot be greater than the number of data points.');
+		}
 	}
 }
