a357dc2465d2f881b20c0109d6d6dd3434147850
RebendaJiri <jr.rebenda@rje.cz>
2015-04-21 15:27:39 +0200

    initial commit

---
+++ src/JR/KMeans/KMeans.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JR\KMeans;
+
+use Nette;
+
+/**
+ * Description of KMeans.
+ *
+ * @author	RebendaJiri
+ * @package JR\KMeans
+ */
+class KMeans
+{
+	/**
+	 * @param int $clusterCount >= 2
+	 * @return array
+	 */
+	public function cluster($clusterCount)
+	{
+		
+	}
+}
