0deb448123380b8034900332dfa6754a1af79be6
RebendaJiri <jr.rebenda@rje.cz>
2015-04-22 18:53:17 +0200

    implementation, example

--- examples/index.php
+++ examples/index.php
@@ -1,17 +1,138 @@
+<?php
+
+require __DIR__ . '/../src/JR/KMeans/KMeans.php';
+
+use JR\KMeans\KMeans;
+
+$data = array(
+	array(1, 2),
+	array(2, 1),
+	array(3, 3),
+	array(1, 2),
+	array(4, 3),
+	array(2, 2),
+	array(6, 9),
+	array(1, 1),
+	array(10, 6),
+	array(5, 2),
+	array(10, 9),
+	array(7, 8),
+);
+//$data = array();
+//for ($i = 0; $i < 20; $i++) {
+//	$row = array();
+//	for ($j = 0; $j < 2; $j++) {
+//		$row[] = mt_rand(1, 50);
+//	}
+//	$data[] = $row;
+//}
+$clusterCount = 2;
+
+$kMeans = new KMeans($data);
+
+$clusteredData = $kMeans->cluster($clusterCount);
+$plotData = array();
+foreach (array_combine(range(0, $clusterCount - 1), $clusteredData) as $clusterIndex => $cluster) {
+	$plotData[$clusterIndex] = array(
+		'name' => 'Cluster ' . ($clusterIndex + 1),
+		'data' => array(),
+	);
+	foreach ($cluster as $dataPointIndex => $dataPoint) {
+		$plotData[$clusterIndex]['data'][$dataPointIndex] = array();
+		foreach ($dataPoint as $val) {
+			$plotData[$clusterIndex]['data'][$dataPointIndex][] = $val;
+		}
+	}
+}
+$plotData = json_encode($plotData);
+//print_r($plotData);
+//exit;
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="windows-1250">
-        <title></title>
-    </head>
-    <body>
-		<?php
-		// put your code here
-		?>
-    </body>
+	<head>
+		<meta charset="UTF-8">
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script src="http://code.highcharts.com/highcharts.js"></script>
+	</head>
+	<body>
+		<!--<table>
+		<?php foreach ($data as $dataPoint): ?>
+			<tr>
+				<?php foreach ($dataPoint as $val): ?>
+				<td><?php echo $val; ?></td>
+				<?php endforeach; ?>
+			</tr>
+		<?php endforeach; ?>
+		</table>-->
+		
+		<div id="chartdiv" style="width: 900px; height: 500px;"></div>
+		
+		<script type="text/javascript">
+			$(function () {
+				$('#chartdiv').highcharts({
+					chart: {
+						type: 'scatter',
+						zoomType: 'xy'
+					},
+					title: {
+						text: 'Height Versus Weight of 507 Individuals by Gender'
+					},
+//					subtitle: {
+//						text: 'Source: Heinz  2003'
+//					},
+					xAxis: {
+						title: {
+							enabled: true,
+							text: 'x'
+						},
+						startOnTick: true,
+						endOnTick: true,
+						showLastLabel: true
+					},
+					yAxis: {
+						title: {
+							text: 'y'
+						}
+					},
+					legend: {
+						layout: 'vertical',
+						align: 'left',
+						verticalAlign: 'top',
+						x: 100,
+						y: 70,
+						floating: true,
+						backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+						borderWidth: 1
+					},
+					plotOptions: {
+						scatter: {
+							marker: {
+								radius: 5,
+								states: {
+									hover: {
+										enabled: true,
+										lineColor: 'rgb(100,100,100)'
+									}
+								}
+							},
+							states: {
+								hover: {
+									marker: {
+										enabled: false
+									}
+								}
+							},
+							tooltip: {
+								headerFormat: '<b>{series.name}</b><br>',
+								pointFormat: '{point.x} cm, {point.y} kg'
+							}
+						}
+					},
+					series: <?php echo $plotData; ?>
+				});
+			});
+		</script>
+	</body>
 </html>
--- src/JR/KMeans/KMeans.php
+++ src/JR/KMeans/KMeans.php
@@ -15,8 +15,14 @@ class KMeans
 	/** @var array */
 	private $data;
 	
+	/** @var array */
+	private $clusteredData = array();
+	
 	public function __construct(array $data)
 	{
+		if (count($data) < 2) {
+			throw new \Exception('At least two data points needed.');
+		}
 		$this->data = $data;
 	}
 	
@@ -29,16 +35,117 @@ class KMeans
 		if ($clusterCount < 2) {
 			throw new \Exception('Cluster count must be equal or greater to 2.');
 		}
-		if ($clusterCount > count($data)) {
+		if ($clusterCount > count($this->data)) {
 			throw new \Exception('Cluster count cannot be greater than the number of data points.');
 		}
+		
+		do {
+			if (empty($centroids)) {
+				$centroids = $this->getInitialCentroids($clusterCount);
+			} else {
+				$centroids = $this->getCentroids($this->clusteredData);
+			}
+			
+			$newClusteredData = array();
+			for ($i = 0; $i < $clusterCount; $i++) {
+				$newClusteredData[$i] = array();
+			}
+			
+			foreach ($this->data as $dataPoint) {
+				$closestCentroid = $this->getClosestCentroid($dataPoint, $centroids);
+				$newClusteredData[$closestCentroid][] = $dataPoint;
+			}
+		} while ($this->check($this->clusteredData, $newClusteredData) === FALSE);
+		
+		return $this->clusteredData;
+	}
+	
+	private function check(array $clusteredData, array $newClusteredData)
+	{
+		if (empty($clusteredData)) {
+			$this->clusteredData = $newClusteredData;
+			return FALSE;
+		}
+		
+		foreach ($clusteredData as $key => $cluster) {
+			foreach ($cluster as $dataPoint) {
+				if (!in_array($dataPoint, $newClusteredData[$key])) {
+					$this->clusteredData = $newClusteredData;
+					return FALSE;
+				}
+			}
+		}
+		
+		return TRUE;
 	}
 	
 	/**
 	 * @return array
 	 */
-	private function getInitialCentroids()
+	private function getInitialCentroids($clusterCount)
 	{
-		
+		$randomKeys = array_flip(array_rand($this->data, $clusterCount));
+		$centroids = array_intersect_key($this->data, $randomKeys);
+		return array_combine(range(0, $clusterCount - 1), $centroids); // remap array keys
+	}
+	
+	/**
+	 * @param array $clusteredData
+	 * @return array
+	 */
+	private function getCentroids(array $clusteredData)
+    {
+        $centroids = array();
+        foreach ($clusteredData as $cluster) {
+            $clusterSum = array_fill(0, count(current($cluster)), 0);
+            foreach ($cluster as $dataPoint) {
+                foreach ($dataPoint as $key => $value) {
+                    $clusterSum[$key] += $value;
+                }
+            }
+            $centroid = array_fill(0, count(current($cluster)), 0);
+            foreach ($clusterSum as $key => $value) {
+                $centroid[$key] = $value / count($cluster);
+            }
+            array_push($centroids, $centroid);
+        }
+        return $centroids;
+    }
+	
+	/**
+	 * Returns centroid closest to given data point.
+	 * 
+	 * @param array $dataPoint
+	 * @param array $centroids
+	 * @return array
+	 */
+	private function getClosestCentroid(array $dataPoint, array $centroids)
+	{
+		$closestDistance = NULL;
+		$closestCentroid = NULL;
+		foreach ($centroids as $centroidKey => $centroid) {
+			$tempDistance = $this->getDistance($dataPoint, $centroid);
+			if ($closestCentroid === NULL || $tempDistance < $closestDistance) {
+				$closestDistance = $tempDistance;
+				$closestCentroid = $centroidKey;
+			}
+		}
+		return $closestCentroid;
+	}
+	
+	/**
+	 * Returns euclidean distance between two datapoints.
+	 * 
+	 * @param array $dataPoint1
+	 * @param array $dataPoint2
+	 * @return float
+	 */
+	private function getDistance(array $dataPoint1, array $dataPoint2)
+	{
+		$sumSquares = 0;
+		for ($i = 0; $i < count($dataPoint1); $i++) {
+			$sumSquares += pow($dataPoint1[$i] - $dataPoint2[$i], 2);
+		}
+		return sqrt($sumSquares);
 	}
 }
